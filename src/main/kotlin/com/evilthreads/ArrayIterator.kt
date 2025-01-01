2d4c090aac88929825e57dc76279db6ed9d875d0
chrisbasinger <evilthreads669966@gmail.com>
2025-01-01 12:45:36 -0600

    add ArrayIterator

---
+++ src/main/kotlin/com/evilthreads/ArrayIterator.kt
@@ -0,0 +1,9 @@
+package com.evilthreads
+
+class ArrayIterator<T>(private val array: Array<T?>): Iterator<T> {
+    private var index = -1
+
+    override fun hasNext(): Boolean = array[index + 1] != null
+
+    override fun next(): T = array[++index]!!
+}
